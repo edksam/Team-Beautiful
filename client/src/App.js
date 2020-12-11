49db4f169ffcad5977c2aa3d1b2c78fdbdb600a1
Jacques Vertommen <jacquesvertommen@gmail.com>
2020-12-11 20:59:15 +0000

    update app.js

--- client/src/App.js
+++ client/src/App.js
@@ -81,7 +81,8 @@ const Main = () => {
       <Route exact path="/" component={Home} />
       <Route exact path="/graduates" component={GraduateList} />
       <Route exact path="/graduates/new" component={GraduateAdd} />
-      <Route exact path="/graduates/:_id" component={PreviewProfile} />
+      <Route exact path="/graduates/profile" component={PreviewProfile} />
+      <Route exact path="/graduates/:_id" component={GraduateProfile} />
       <Route exact path="/graduates/:_id/edit" component={GraduateEdit} />
     </Switch>
   );
